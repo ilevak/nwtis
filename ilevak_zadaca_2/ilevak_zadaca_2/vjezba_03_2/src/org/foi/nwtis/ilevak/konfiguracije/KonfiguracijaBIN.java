489408958889fb181862d031b888970ad5a07016
Iva Levak <37185799+ilevak@users.noreply.github.com>
2020-12-27 17:14:51 +0100

    Add files via upload

---
+++ ilevak_zadaca_2/ilevak_zadaca_2/vjezba_03_2/src/org/foi/nwtis/ilevak/konfiguracije/KonfiguracijaBIN.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.foi.nwtis.ilevak.konfiguracije;
+
+/**
+ *
+ * @author NWTiS_1
+ */
+public class KonfiguracijaBIN extends KonfiguracijaApstraktna {
+
+    public KonfiguracijaBIN(String datoteka) {
+        super(datoteka);
+    }
+
+    @Override
+    public void ucitajKonfiguraciju() throws NemaKonfiguracije, NeispravnaKonfiguracija {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void ucitajKonfiguraciju(String datoteka) throws NemaKonfiguracije, NeispravnaKonfiguracija {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void spremiKonfiguraciju() throws NemaKonfiguracije, NeispravnaKonfiguracija {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void spremiKonfiguraciju(String datoteka) throws NemaKonfiguracije, NeispravnaKonfiguracija {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+   
+}
